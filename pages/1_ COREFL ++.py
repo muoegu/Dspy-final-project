35a51138babde2de626f2b5626512baf973ba398
Micah Kitsunai <kmicah1794@gmail.com>
2023-12-16 17:45:12 +0800

    added charts

--- pages/1_ COREFL ++.py
+++ pages/1_ COREFL ++.py
@@ -56,7 +56,7 @@ with col1:
     df1 = filter_df(df, 'Medium', ('All','Written', 'Spoken'), 'All', 'selectbox1')
     df1 = filter_df(df1, 'Sex', ('All','Male', 'Female'), 'All', 'selectbox2')
     df1 = filter_df(df1, 'L1', ('All','German', 'Spanish'), 'All', 'selectbox3')
-    df1 = filter_df(df1, 'Proficiency', ('All','intermediate', 'advanced'), 'All', 'selectbox4')
+    df1 = filter_df(df1, 'Proficiency_Category', ('All','intermediate', 'advanced'), 'All', 'selectbox4')
     # df1 = filter_df(df1, 'Year data collection', ('All','2017', '2018', '2019', '2020', '2021'), 'All', 'selectbox5')
     selected_columns = st.multiselect('Choose columns:', df1.columns, key='multiselect1')
     filtered_df1 = df1[selected_columns + ['word_counts']+ ['pos_counts']]
@@ -70,7 +70,7 @@ with col2:
     df2 = filter_df(df, 'Medium', ('All','Written', 'Spoken'), 'All', 'selectbox11')
     df2 = filter_df(df2, 'Sex', ('All','Male', 'Female'), 'All', 'selectbox12')
     df2 = filter_df(df2, 'L1', ('All','German', 'Spanish'), 'All', 'selectbox13')
-    df2 = filter_df(df2, 'Proficiency', ('All','intermediate', 'advanced'), 'All', 'selectbox14')
+    df2 = filter_df(df2, 'Proficiency_Category', ('All','intermediate', 'advanced'), 'All', 'selectbox14')
     # df2 = filter_df(df2, 'Year data collection', ('All','2017', '2018', '2019', '2020', '2021'), 'all', 'selectbox15')
     selected_columns = st.multiselect('Choose columns:', df2.columns, key='multiselect2')
     filtered_df2 = df2[selected_columns + ['word_counts']+ ['pos_counts']]
@@ -102,11 +102,15 @@ result2 = Count_sum(filtered_df2, count_option2)
 
 
 
-
 def create_df(result):
-    count_df = pd.DataFrame.from_dict(result, orient='index', columns=['count'])
+    pos = list(result.keys())
+    counts = list(result.values())
+
+    count_df = pd.DataFrame({'POS or Word': pos, 'count': counts})
+
     count_df['percentage'] = (count_df['count'] / count_df['count'].sum()) * 100
     count_df['percentage'] = count_df['percentage'].round(2)
+
     return count_df
 
 
@@ -122,20 +126,61 @@ if __name__ == "__main__":
         st.write("Result 1", result1_df)
     with col2:
         st.write("Result 2", result2_df)
-        
+
+
+head_num = st.selectbox(
+            'Select count option:',
+            (5, 10, 15, 20), key='selectbox30')
+
+
+merged_df = pd.merge(result1_df[['POS or Word', 'percentage']], result2_df[['POS or Word', 'percentage']], on='POS or Word', suffixes=('_a', '_b'))
+merged_df = merged_df.sort_values(by=['percentage_a', 'percentage_b'], ascending=False).head(head_num)
+
+# インデックス（POS or Word）をカラムに変換してから、棒グラフを作成
+merged_df.set_index('POS or Word', inplace=True)
+merged_df[['percentage_a', 'percentage_b']].plot(kind='bar', figsize=(12, 6))
+
+st.title("Charts")
+
+
+plt.title('Percentage Comparison by POS or Word')
+plt.ylabel('Percentage(%)')
+plt.xlabel('POS or Word')
+plt.legend(['Dataset A', 'Dataset B'])
+plt.xticks(rotation=45)
+st.pyplot(plt)
+
+total_a = merged_df['percentage_a'].sum()
+total_b = merged_df['percentage_b'].sum()
+fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+axs[0].pie(merged_df['percentage_a'], labels=merged_df.index, autopct=lambda p: '{:.1f}%'.format(p * total_a / 100), startangle=140)
+axs[0].set_title('Dataset A Percentage')
+axs[1].pie(merged_df['percentage_b'], labels=merged_df.index, autopct=lambda p: '{:.1f}%'.format(p * total_b / 100), startangle=140)
+axs[1].set_title('Dataset B Percentage')
+st.pyplot(plt)
+
+
+merged_df[['percentage_a', 'percentage_b']].plot(kind='bar', stacked=True, figsize=(12, 6))
+plt.title('Stacked Percentage Comparison by POS or Word')
+plt.ylabel('Percentage(%)')
+plt.xlabel('POS or Word')
+plt.legend(['Dataset A', 'Dataset B'])
+plt.xticks(rotation=45)
+st.pyplot(plt)
+
+
 
 
 def display_bar_chart():
-    top_15_1 = result1_df.sort_values('percentage', ascending=False).head(15)
-    top_15_2 = result2_df.sort_values('percentage', ascending=False).head(15)
-    
+    top_15_1 = result1_df.sort_values('percentage', ascending=False).head(head_num)
+    top_15_2 = result2_df.sort_values('percentage', ascending=False).head(head_num) #.head(15)???
     
     col1, col2 = st.columns(2)
 
     with col1:
         plt.figure(figsize=(12, 8))
-        plt.bar(top_15_1.index, top_15_1['percentage'])
-        plt.xlabel('aaa')
+        plt.bar(top_15_1['POS or Word'], top_15_1['percentage'])
+        plt.xlabel('POS or Word')
         plt.ylabel('percentage')
         plt.title(' mksladgdnkjla by Percentage')
         plt.xticks(rotation=45)
@@ -144,8 +189,8 @@ def display_bar_chart():
         
     with col2:      
         plt.figure(figsize=(12, 8))
-        plt.bar(top_15_2.index, top_15_2['percentage'])
-        plt.xlabel('mnskl')
+        plt.bar(top_15_2['POS or Word'], top_15_2['percentage'])
+        plt.xlabel('POS or Word')
         plt.ylabel('percentage')
         plt.title('jwil by Percentage')
         plt.xticks(rotation=45)
@@ -153,7 +198,6 @@ def display_bar_chart():
         st.pyplot(plt)
 
 if __name__ == "__main__":
-    st.title("Bar chart")
     display_bar_chart()
 
 
